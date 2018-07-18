18c9ae16f2f1a0e44403616b8e3ada7c6d538aad
h8windows <h8windows@gmail.com>
2018-07-18 14:18:57 -0700

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'movies#index'
+  resources :movies
+  root :to => redirect('/movies')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,8 +15,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
-  
+
   # Example resource route with options:
   #   resources :products do
   #     member do
