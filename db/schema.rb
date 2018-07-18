a66614d349ee2c8101831849386b9da744e49455
h8windows <h8windows@gmail.com>
2018-07-18 16:57:33 -0700

    removed sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20180718232557) do
+
+  create_table "moviegoers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
