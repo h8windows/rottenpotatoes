33bea203a81dbe2b5a536631eca57b90f352ea42
h8windows <h8windows@gmail.com>
2018-07-18 15:43:52 -0700

    Working on localhost

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,41 +10,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
-    
-    @all_ratings = Movie.ratings
-    session[:sort_by] ||= 'id'
-    session[:ratings] ||= Hash[@all_ratings.map {|rating| [rating,1]}]
-    if params[:ratings].nil?
-      @ratings_desired = session[:ratings].keys
-    else 
-      @ratings_desired = params[:ratings].keys
-      # store the info for the next time
-      session[:ratings] = params[:ratings]
-    end
-    if params[:sort_by].nil?
-      @sort = session[:sort_by]
-    else 
-      @sort = params[:sort_by]
-      session[:sort_by] = params[:sort_by]
-    end
-    
-    if params[:sort_by].nil? || params[:ratings].nil?
-      flash.keep
-      # see rake routes (movies path is controlled by index)
-      redirect_to movies_path(ratings: session[:ratings], sort_by: session[:sort_by])
-    end
-    
-    
-    if @sort == "title"
-      @title_hilite = "hilite"
-    elsif @sort == "release_date"
-      @date_hilite = "hilite"
-    end
-    
-    
-    @movies = Movie.where(rating: @ratings_desired).order(@sort)
-    
+    @movies = Movie.all.order(:title)
   end
 
   def new
