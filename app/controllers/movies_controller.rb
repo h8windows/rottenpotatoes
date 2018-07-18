5edc920bc4105f3987ad79acf4b0a412296e35ae
h8windows <h8windows@gmail.com>
2018-07-18 14:42:16 -0700

    index complete with partial

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,57 +1,78 @@
 class MoviesController < ApplicationController
+
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
-  def index
-    @movies = Movie.all.order(:title)
-  end
   def show
-    begin
-      @movie = Movie.find params[:id]
-    rescue
-      redirect_to movies_path
-    end
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
   end
-  def new
-    @movie = Movie.new
-  end
-  def create
 
-    @movie = Movie.create(movie_params)
-    if @movie.save
-      flash[:notice] = "#{@movie.title} was successfully created."
-      redirect_to movie_path(@movie)
-    else
-      render 'new'
+  def index
+    @movies = Movie.all
+    
+    @all_ratings = Movie.ratings
+    session[:sort_by] ||= 'id'
+    session[:ratings] ||= Hash[@all_ratings.map {|rating| [rating,1]}]
+    if params[:ratings].nil?
+      @ratings_desired = session[:ratings].keys
+    else 
+      @ratings_desired = params[:ratings].keys
+      # store the info for the next time
+      session[:ratings] = params[:ratings]
     end
+    if params[:sort_by].nil?
+      @sort = session[:sort_by]
+    else 
+      @sort = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    end
+    
+    if params[:sort_by].nil? || params[:ratings].nil?
+      flash.keep
+      # see rake routes (movies path is controlled by index)
+      redirect_to movies_path(ratings: session[:ratings], sort_by: session[:sort_by])
+    end
+    
+    
+    if @sort == "title"
+      @title_hilite = "hilite"
+    elsif @sort == "release_date"
+      @date_hilite = "hilite"
+    end
+    
+    
+    @movies = Movie.where(rating: @ratings_desired).order(@sort)
+    
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
   end
 
   def edit
-    begin
-      @movie = Movie.find params[:id]
-    rescue
-      redirect_to movies_path
-    end
+    @movie = Movie.find params[:id]
   end
 
   def update
     @movie = Movie.find params[:id]
-    if @movie.update_attributes(movie_params)
-      flash[:notice] = "#{@movie.title} was successfully updated"
-      respond_to do |client_wants|
-        client_wants.html { redirect_to movie_path(@movie) }
-        client_wants.xml { render :xml => @movie.to_xml }
-      end
-    else
-      render 'edit'
-    end
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
   end
 
   def destroy
-    @movie = Movie.find params[:id]
+    @movie = Movie.find(params[:id])
     @movie.destroy
-    flash[:notice] = "#{@movie.title} was successfully deleted"
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+
 end
