9a78257f35906f38b2599abeca3feb82498b7c65
h8windows <h8windows@gmail.com>
2018-07-19 13:42:18 -0700

    added keys to secrets.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "VY7tCrzp31rgTfL0BhRMIpEQ5","0NqVLok5VKx4LwDMWQoJbR5L6JJVD5W1qZHwS0BJL7zfJTcwfv"
+  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end
