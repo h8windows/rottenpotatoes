7684f99f1fb5b8f6cc4715f3c18984e3ff5d0e01
h8windows <h8windows@gmail.com>
2018-07-19 14:40:53 -0700

    fixed syntax in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root :to => redirect('/movies')
 
   get '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/failure', to: '/movies'
+  get '/auth/failure' => 'movies#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
