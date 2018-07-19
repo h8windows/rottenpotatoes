e4d4cbc31ca32766bf88bbf2e6ef96094d5dce42
h8windows <h8windows@gmail.com>
2018-07-19 14:19:06 -0700

    Added auth/twitter to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :movies
   root :to => redirect('/movies')
 
+  get '/auth/:provider/callback', to: 'sessions#create'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
