cb84c6a02da1ded621339c34c8a6593485d4eaa6
h8windows <h8windows@gmail.com>
2018-07-20 12:36:45 -0700

    commented redirect in application controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,7 +31,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    redirect_to movies_path(@movie)
   end
 
   def destroy
